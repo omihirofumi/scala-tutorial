3521c5ce9fb4a2abe1655ef71d25ec119909139d
hirofumi omi <omihirofumi@gmail.com>
2023-09-03 16:53:53 +0900

    add main

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,3 +1,13 @@
-object Hello extends App {
-  println("Hello, World!")
-}
+import scala.collection.mutable.ListBuffer
+
+def double(ints :List[Int]): List[Int] =
+  var buffer = new ListBuffer[Int]()
+  for i <- ints do
+    buffer += i*2
+  buffer.toList
+
+var oldNumbers = List(1, 2, 3)
+val newNumbers = double(oldNumbers)
+
+@main def test() =
+  println(newNumbers)
---
+++ src/main/scala/example/Main.scala
@@ -0,0 +1,3 @@
+@main def run() =
+  val ages = Seq(42, 61, 31, 64)
+  println(s"The oldest person is ${ages.max}")
